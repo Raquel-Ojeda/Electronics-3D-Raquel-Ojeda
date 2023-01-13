03d88121e2bfb2d7390dd49504c4bc738f58a7b8
Raquel <electronics3dmx@gmail.com>
2023-01-12 20:23:32 -0600

    asignamos funciones de consola

--- index.js
+++ index.js
@@ -1,71 +1,48 @@
-// Variables
-// Globales 
-var saludo ="Hola a todos";
-if(saludo){
-    var saludo = "Hola México, Europa, USA o reptiliano";
-    console.log(saludo);
+
+//--- enero
+//Sumatoria
+const sumatoria = function (x,y,z){
+    return x+y+z ;
+}
+let c=25
+let d=12
+let e=213
+console.log(sumatoria(c , d , e));
+// Comparación con funciones 
+
+let passwordFiltro = 255;
+
+const login = function (x){
+    return x;
 }
 
-//Limitadas
-let nombre="Raquel";
-if (nombre){
-    let nombre = "Reynaldo";
-    console.log(nombre);
+login(passwordFiltro) == 255 ? console.log('Bienvenido') : console.log('Denegado');
+
+//flecha
+const acceso =(clave, usuario)=> console.log(`Usuario ${usuario} su acceso es ${clave}`);
+acceso(passwordFiltro, "Raquel")
+
+//Objetos
+const cliente1 = {
+    //clave: valor
+    nombre: 'Raúl',
+    email: 'raul2@gmail.com',
+    pais: 'Madrid',
+    acceso: true,
+    pago: 'Transferencia',
 }
-console.log(nombre);
+console.log(cliente1);
 
-//Inmutables
-const rango="admins";
-console.log(rango);
+const {nombre, email, pago} = cliente1;
+console.log(nombre, email, pago);
 
-//Tipo de variables 
-//STRING
-let cargo ="Raquel Ojeda- CEO";
-console.log(cargo);
+//Array 
+let access = true 
+const primerArray = ['Raúl', 38, access, sumatoria(c,d), ['Madrid',2023]];
 
-//Number
-let edad=23;
-console.log(edad);
+//const [nambre, edad, año] = primerArray;
+//console.log(nambre,edad, año);
 
-//Boolean
-let casada=true;
-console.log(casada);
-
-//Undefined
-let fechaDeEmbarazo=undefined;
-console.log(fechaDeEmbarazo);
-
-//Null
-let numeroDeHijos=null;
-console.log(numeroDeHijos);
-
-//Concatenar
-let nombreUsuario = prompt(`¡Hola! Bienvenido, ¿cuál es tu nombre?`);
-alert(`Hola " + nombreUsuario + " " + "¿Cómo te enteraste de nosotos?, escríbenos al WhatsApp.`);
-let edadUsuario = prompt("¿Cuántos años tienes?");
-
-
-//Interpolación 
-alert(`Oh mira, ${nombreUsuario} tienes ${edadUsuario} años, disfruta de nuestra web.`);
-
-//OPERADORES
-
-let a = 5
-let b = 32
- //Operadores aritméticos
-console.log(a*b);
-//Asignación con operación aritmética
-console.log(a=b);
-console.log(a+=b);
-console.log(a/b);
-//Comparación
-
-if (edadUsuario>=18){
-alert(`Venga ${nombreUsuario} toma un trago, la casa invita`);
-}
-else{
-    alert(`¡Alto ahí ${nombreUsuario}! Ten, toma ${edadUsuario * 2} chocolates`);
-}
-if(edadUsuario==18){
-    alert(`¡ALTOOOOO AHÍ ${nombreUsuario}! Venga que sos recién legal, toma ${edadUsuario/6} shots de alegría y ve a disfrutar de nuestra web`)
-}
+const functionArray = () => { return primerArray; }
+const [nambre, edad, ingreso]=functionArray();
+console.log(nambre,edad,ingreso);
---
+++ indexPrimero.js
@@ -0,0 +1,72 @@
+// Variables
+// Globales 
+var saludo ="Hola a todos";
+if(saludo){
+    var saludo = "Hola México, Europa, USA o reptiliano";
+    console.log(saludo);
+}
+
+//Limitadas
+let nombre="Raquel";
+if (nombre){
+    let nombre = "Reynaldo";
+    console.log(nombre);
+}
+console.log(nombre);
+
+//Inmutables
+const rango="admins";
+console.log(rango);
+
+//Tipo de variables 
+//STRING
+let cargo ="Raquel Ojeda- CEO";
+console.log(cargo);
+
+//Number
+let edad=23;
+console.log(edad);
+
+
+//Boolean
+let casada=true;
+console.log(casada);
+
+//Undefined
+let fechaDeEmbarazo=undefined;
+console.log(fechaDeEmbarazo);
+
+//Null
+let numeroDeHijos=null;
+console.log(numeroDeHijos);
+
+//Concatenar
+let nombreUsuario = prompt(`¡Hola! Bienvenido, ¿cuál es tu nombre?`);
+alert(`Hola " + nombreUsuario + " " + "¿Cómo te enteraste de nosotos?, escríbenos al WhatsApp.`);
+let edadUsuario = prompt("¿Cuántos años tienes?");
+
+
+//Interpolación 
+alert(`Oh mira, ${nombreUsuario} tienes ${edadUsuario} años, disfruta de nuestra web.`);
+
+//OPERADORES
+
+let a = 5
+let b = 32
+ //Operadores aritméticos
+console.log(a*b);
+//Asignación con operación aritmética
+console.log(a=b);
+console.log(a+=b);
+console.log(a/b);
+//Comparación
+
+if (edadUsuario>=18){
+alert(`Venga ${nombreUsuario} toma un trago, la casa invita`);
+}
+else{
+    alert(`¡Alto ahí ${nombreUsuario}! Ten, toma ${edadUsuario * 2} chocolates`);
+}
+if(edadUsuario==18){
+    alert(`¡ALTOOOOO AHÍ ${nombreUsuario}! Venga que sos recién legal, toma ${edadUsuario/6} shots de alegría y ve a disfrutar de nuestra web`)
+}
