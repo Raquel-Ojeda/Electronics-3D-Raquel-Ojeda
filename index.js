2e0525314b6bb53876279a6c6dae6505d6a52a53
Raquel <electronics3dmx@gmail.com>
2023-01-29 00:02:26 -0600

    Trabajamos en js y de manera responsiva todas las pestañas

--- index.js
+++ index.js
@@ -47,22 +47,22 @@ const functionArray = () => { return primerArray; }
 const [nambre, edad, ingreso]=functionArray();
 console.log(nambre,edad,ingreso);
 //Mensajes con condicional 
-let nombreUsuario = prompt(`¡Hola! Bienvenido, ¿cuál es tu nombre?`);
-alert(`Hola " + nombreUsuario + " " + "¿Cómo te enteraste de nosotos?, escríbenos al WhatsApp.`);
+/*let nombreUsuario = prompt(`¡Hola! Bienvenido, ¿cuál es tu nombre?`);
+alert(`Hola  ${nombreUsuario}  ¿Cómo te enteraste de nosotros?, escríbenos al WhatsApp.`);
 let edadUsuario = prompt("¿Cuántos años tienes?");
 
 
 if (edadUsuario>=18){
-    alert(`Venga ${nombreUsuario} toma un trago, la casa invita`);
+    alert(`Venga ${nombreUsuario} tienes el 10% de descuento`);
     }
     else{
-        alert(`¡Alto ahí ${nombreUsuario}! Ten, toma ${edadUsuario * 2} chocolates`);
+        alert(`¡Alto ahí ${nombreUsuario}! Ten, toma ${edadUsuario * 20} gramos de PLA gratis`);
     }
     if(edadUsuario==18){
-        alert(`¡ALTOOOOO AHÍ ${nombreUsuario}! Venga que sos recién legal, toma ${edadUsuario/6} shots de alegría y ve a disfrutar de nuestra web`)
-    }
+        alert(`¡ALTOOOOO AHÍ ${nombreUsuario}! Venga que sos recién legal, toma ${edadUsuario*2} de descuento`)
+    }*/
     //Switch
-switch(descuentos){
+/*switch(descuentos){
     case edadUsuario>=18:
         console.log ('Tienes un 20% de descuento adicional');
         alert(`${nombreUsuario}Tienes un 20% de descuento adicional`)
@@ -78,6 +78,7 @@ switch(descuentos){
         alert(`Además recibe un 25% de descuento adicional`);
         break;
 }//lo intenté :C 
+*/
 /*CarritoArray
 const carrito=[];
 
@@ -109,23 +110,76 @@ const proucto3 ={
 }
 */
 const materiales =[
-    {id: 1 , nombre: "PLA", precio: 300, img:"img.jpg"},
-    {id: 2 , nombre: "PETG", precio: 399, img:"img.jpg"},
-    {id: 3 , nombre: "TPU", precio: 699, img:"img.jpg"},
-    {id: 4 , nombre: "PLA GLOW", precio: 699, img:"img.jpg"},
+    {id: 1 , nombre: "PLA", precio: 300, stock: 10, img:"img.jpg"},
+    {id: 2 , nombre: "PETG", precio: 399, stock: 11, img:"img.jpg"},
+    {id: 3 , nombre: "TPU", precio: 699, stock: 8, img:"img.jpg"},
+    {id: 4 , nombre: "PLA GLOW", precio: 699, stock: 15, img:"img.jpg"},
     
 ];
+let suma=0;
 
-for (let i=0; i<comidas.length; i++){
-    console.log("Total de productos" , suma+= comidas [i].precio);
-}
 const carrito=[];
-const agregarAlCarrito = (producto, carrito)=>{
-    carrito.push(producto);
-    console.log("Se agrego con éxito al carrito");
-}
-agregarAlCarrito(producto[1], carrito);
-agregarAlCarrito(producto[2], carrito);
-agregarAlCarrito(producto[3], carrito);
 
-console.log("Carrito de compras", carrito)
+const agregarAlCarrito = (material, carrito)=>[
+    carrito.push(material),
+    console.log("Se agrego con éxito al carrito"),
+];
+
+/*for (let i=0; i<materiales.length; i++){
+    console.log("Total de productos" , suma = materiales [i].precio);
+}
+*/
+
+materiales.forEach (materiales => {
+    console.log(materiales.nombre, materiales.stock)
+});
+
+const materialesFiltrado = materiales.filter(item => item.precio >300);
+console.log("Los materiales más caros son:", materialesFiltrado);
+
+let nuevoArray= materiales.map((item)=>({
+    nombre: item.nombre,
+    precio: item.precio / 2,
+    id: item.id + 1
+}));
+console.log(materiales, "Array sin descuento");
+console.log(nuevoArray,"Array con descuento");
+
+agregarAlCarrito(materiales[1], carrito);
+agregarAlCarrito(materiales[2], carrito);
+agregarAlCarrito(materiales[3], carrito);
+agregarAlCarrito(materiales[4], carrito);
+
+console.log ("El carrito actualmente tiene :", carrito);
+/*
+const arrayPrecio = materiales.map (item => item.precio);
+
+console.log (arrayPrecio , "nuevo array solo con precios");*/
+
+//const precioPromedio = carrito.reduce ((total , precioProductos)=> total + precioProductos.precio, 560);
+//console.log("El valor promedio es : ", precioPromedio);
+//lo intente pero no se por qué me aparece anonymous
+
+const ordenAlf = materiales.sort((item1,item2)=> {
+    if(item1.nombre===item2.nombre){
+        return 0
+    } else if(item1.nombre <item2.nombre){
+        return-1;
+    }else{
+        return 1
+    }
+});
+
+console.log("Orden alfabético", ordenAlf);
+
+const Costoso = materiales.sort((item1,item2)=> {
+    if(item1.precio===item2.precio){
+        return 0
+    } else if(item1.precio >item2.precio){
+        return-1;
+    }else{
+        return 1
+    }
+});
+
+console.log("El producto más costoso es: ", ordenAlf);
